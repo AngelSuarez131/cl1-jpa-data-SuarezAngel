447ddde011e1dc84823d86db567f07a8172aeaa8
LILIANA CAZORLA <angelsuarez131@gmail.com>
2024-11-20 02:35:51 -0500

    primer

---
+++ src/main/java/pe/edu/_0/cl1_jpa_data_SuarezAngel/Cl1JpaDataSuarezAngelApplication.java
@@ -0,0 +1,13 @@
+package pe.edu._0.cl1_jpa_data_SuarezAngel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JpaDataSuarezAngelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JpaDataSuarezAngelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/_0/cl1_jpa_data_SuarezAngel/Cl1JpaDataSuarezAngelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu._0.cl1_jpa_data_SuarezAngel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JpaDataSuarezAngelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
